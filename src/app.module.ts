9493123602048af340398ce6466b3d3fd91589f5
shimjaedoo <woen1637@naver.com>
2024-06-09 14:28:44 +0900

    chore : imports module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MemberModule } from './member/member.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { BcryptModule } from './bcrypt/bcrypt.module';
+import { JwtModule } from './jwt/jwt.module';
+import { AuthModule } from './auth/auth.module';
+import { BoardModule } from './board/board.module';
+import { CommentModule } from './board/comment/comment.module';
 
 @Module({
-  imports: [],
+  imports: [MemberModule, PrismaModule, BcryptModule, JwtModule, AuthModule, BoardModule, CommentModule],
   controllers: [AppController],
   providers: [AppService],
 })
